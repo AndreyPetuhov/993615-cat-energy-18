d79cebffb6cabac73343e0c684039b77f1231cfb
AndreyPetuhov <sir.andruha@yandex.ru>
2019-12-12 10:20:15 +0300

    исправил ошибки в миксинах и JS

--- source/js/script.js
+++ source/js/script.js
@@ -1,38 +1,23 @@
 /*кнопка открытия меню*/
 var mainNav = document.querySelector(".main-nav");
-var mainNavWrapper = document.querySelector(".main-nav__wrapper");
 var mainNavButton = document.querySelector(".main-nav__button");
 
 mainNavButton.addEventListener("click", function () {
-  if (mainNav.classList.contains('main-nav--closed')) {
-    mainNav.classList.remove('main-nav--closed');
-    mainNav.classList.add('main-nav--opened');
-  } else {
-    mainNav.classList.add('main-nav--closed');
-    mainNav.classList.remove('main-nav--opened');
-  }
+  mainNav.classList.toggle("main-nav--closed");
+  mainNav.classList.toggle("main-nav--opened");
 });
 
 /*слайдер*/
-
-var slideBefore = document.querySelector(".slider__image--before");
-var slideAfter = document.querySelector(".slider__image--after");
+var slider = document.querySelector(".slider");
 var buttonBefore = document.querySelector(".slider__toggle--before");
 var buttonAfter = document.querySelector(".slider__toggle--after");
-var scaleLeft = document.querySelector(".slider__scale--left");
-var scaleRight = document.querySelector(".slider__scale--right");
-
 
 buttonBefore.addEventListener("click", function () {
-  slideBefore.classList.remove('hidden');
-  slideAfter.classList.add('hidden');
-  scaleLeft.classList.remove('opacity');
-  scaleRight.classList.add('opacity');
+  slider.classList.remove("slider--after");
+  slider.classList.add("slider--before");
 });
 
 buttonAfter.addEventListener("click", function () {
-  slideAfter.classList.remove('hidden');
-  slideBefore.classList.add('hidden');
-  scaleRight.classList.remove('opacity');
-  scaleLeft.classList.add('opacity');
+  slider.classList.remove("slider--before");
+  slider.classList.add("slider--after");
 });
