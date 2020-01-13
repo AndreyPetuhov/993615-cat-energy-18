69c9f73598f550f17bd8c09ec9c76bc8db115e7f
AndreyPetuhov <sir.andruha@yandex.ru>
2020-01-13 21:56:48 +0300

    Поправил скрипт формы

--- source/js/script.js
+++ source/js/script.js
@@ -11,30 +11,28 @@ mainNavButton.addEventListener("click", function () {
 
 /*слайдер*/
 var slider = document.querySelector(".slider");
-var buttonBefore = document.querySelector(".slider__toggle--before");
-var buttonAfter = document.querySelector(".slider__toggle--after");
 
-buttonBefore.addEventListener("click", function () {
-  slider.classList.remove("slider--after");
-  slider.classList.add("slider--before");
-});
+if (slider) {
+  var buttonBefore = document.querySelector(".slider__toggle--before");
+  var buttonAfter = document.querySelector(".slider__toggle--after");
 
-buttonAfter.addEventListener("click", function () {
-  slider.classList.remove("slider--before");
-  slider.classList.add("slider--after");
-});
+  buttonBefore.addEventListener("click", function () {
+    slider.classList.remove("slider--after");
+    slider.classList.add("slider--before");
+  });
+
+  buttonAfter.addEventListener("click", function () {
+    slider.classList.remove("slider--before");
+    slider.classList.add("slider--after");
+  });
+}
 
 /*валидация формы*/
 
 var form = document.querySelector(".form");
-var name = popup.querySelector("[name=name]");
-var weight = popup.querySelector("[name=weight]");
-var email = popup.querySelector("[name=email]");
-var tel = popup.querySelector("[name=tel]");
 
-form.addEventListener("submit", function (evt) {
-  if (!name.value || !weight.value || !email.value || !tel.value) {
-    evt.preventDefault();
+if (form) {
+  form.addEventListener("invalid", function (evt) {
     form.classList.add("form--invalid");
-  }
-});
+  }, true);
+}
